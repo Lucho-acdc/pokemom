9c2bf1cd9bd923d5131a560ff20b076333971627
lucho-acdc <lucianohamoroso@gmail.com>
2023-10-08 13:15:44 -0300

    arreglo del boton 16

--- js/main.js
+++ js/main.js
@@ -16,16 +16,15 @@ let clickAudio = new Audio("./sonidos/click.wav");
 let rightAudio = new Audio("./sonidos/right.wav");
 let wrongAudio = new Audio("./sonidos/wrong.wav");
 
-
 let mostrarAciertos = document.getElementById("aciertos");
 let mostrarMovimientos = document.getElementById("tiempoRestante");
 let mostrarTiempo = document.getElementById("movimientos");
 
-let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+let numeros = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]; // Números del 1 al 8, cada uno repetido dos veces
 
-
+// Revolver el arreglo de números para los otros botones
 numeros = numeros.sort(() => {
-    return Math.random() - 0.5
+    return Math.random() - 0.5;
 });
 
 contarTiempo = () => {
@@ -33,7 +32,7 @@ contarTiempo = () => {
         timer--;
         mostrarTiempo.innerHTML = `Tiempo: ${timer} segundos`;
 
-        if(timer === 0) {
+        if (timer === 0) {
             clearInterval(tiempoRegresivoId);
             bloquearTarjetas();
             // audios
@@ -42,39 +41,12 @@ contarTiempo = () => {
 }
 
 bloquearTarjetas = () => {
-    for(let i=1; i<17; i++) {
-        let tarjetaBloqueada = document.getElementById(i);
+    for (let i = 0; i < 16; i++) {
+        let tarjetaBloqueada = document.getElementById(i + 1);
         tarjetaBloqueada.innerHTML = `<img src="./img/${numeros[i]}.png">`;
         tarjetaBloqueada.disabled = true;
     }
 }
-
-// funcion principal original con onclick
-
-// destapar = (id) => {
-//     if(temporizador == false) {
-//         contarTiempo();
-//         temporizador = true;
-//     }
-//     tarjetasDestapadas++;
-
-//     if(tarjetasDestapadas == 1){
-//         tarjeta1 = document.getElementById(id);
-//         primerResultado = numeros[id];
-//         tarjeta1.innerHTML = `<img src="./img/${primerResultado}.png">`;
-//         tarjeta1.disabled = true;
-//     } else if(tarjetasDestapadas == 2){
-//         tarjeta2 = document.getElementById(id);
-//         segundoResultado = numeros[id];
-//         tarjeta2.innerHTML = `<img src="./img/${segundoResultado}.png">`;
-//         tarjeta2.disabled = true;
-//     }
-
-
-// }
-
-
-// funcion principal sin onclick
 
 destapar = (e) => {
     const buttonId = e.target.id;
@@ -86,23 +58,23 @@ destapar = (e) => {
 
     if (tarjetasDestapadas === 1) {
         tarjeta1 = e.target;
-        primerResultado = numeros[buttonId];
+        primerResultado = numeros[buttonId - 1];
         tarjeta1.innerHTML = `<img src="./img/${primerResultado}.png">`;
         tarjeta1.disabled = true;
     } else if (tarjetasDestapadas === 2) {
         tarjeta2 = e.target;
-        segundoResultado = numeros[buttonId];
+        segundoResultado = numeros[buttonId - 1];
         tarjeta2.innerHTML = `<img src="./img/${segundoResultado}.png">`;
         tarjeta2.disabled = true;
         movimientos++;
         mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
 
-        if(primerResultado == segundoResultado){
+        if (primerResultado == segundoResultado) {
             tarjetasDestapadas = 0;
             aciertos++;
             mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
 
-            if(aciertos == 8){
+            if (aciertos == 8) {
                 clearInterval(tiempoRegresivoId);
                 mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
                 mostrarMovimientos.innerHTML = `El total de movimientos fue: ${movimientos}`;
@@ -115,25 +87,15 @@ destapar = (e) => {
                 tarjeta1.disabled = false;
                 tarjeta2.disabled = false;
                 tarjetasDestapadas = 0;
-
             }, 700);
         } 
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('button');
     
     buttons.forEach(button => {
         button.addEventListener('click', destapar);
     });
 });
-
-
-// const buttons = document.querySelectorAll('button');
-// buttons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         const buttonId = button.id;
-//         destapar(id);
-//     });
-// });
